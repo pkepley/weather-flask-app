b84ffc1f483132ce9095489aaa741b0ef28a6be6
Paul Kepley <pakepley@gmail.com>
2020-03-10 21:28:56 -0500

    Further conversion of scripts to udate the db.

--- update_weather_db.py
+++ update_weather_db.py
@@ -3,7 +3,9 @@ import sqlite3
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
+from pytz import timezone
 from forecast_tools import get_avf_compare, get_fcst, get_actl
+from pull_weather import midnight_pull_list
 
 def check_table_exists(db_conn, table_name):
     # Cursor
@@ -71,7 +73,10 @@ def update_nws_actl_db(db_conn, airport_name, df_nws_actl, actl_dt):
     data_loaded = check_nws_actl_loaded(db_conn, airport_name, actl_dt)    
 
     # Load data if not present
-    if not data_loaded:
+    if data_loaded:
+        print('Actl data found in db for {0} on {1}'.format(airport_name, actl_dt))
+    else:
+        print('Updating actl for {0} on {1}'.format(airport_name, actl_dt))        
         df_nws_actl['airport_name'] = airport_name
 
         for c in columns:
@@ -127,7 +132,11 @@ def update_nws_fcst_db(db_conn, airport_name, df_nws_fcst, pull_dt):
     data_loaded = check_nws_fcst_loaded(db_conn, airport_name, pull_dt)    
 
     # Load data if not present
-    if not data_loaded:
+    if data_loaded:
+        print('Fcst data found in db for {0} on {1}'.format(airport_name, pull_dt))
+    else:
+        print('Updating fcst for {0} on {1}'.format(airport_name, pull_dt))        
+        
         df_nws_fcst['airport_name'] = airport_name
 
         for c in columns:
@@ -186,11 +195,16 @@ def update_nws_avf_compare_db(db_conn, airport_name, fcst_date_str):
     data_loaded = check_nws_avf_compare_loaded(db_conn, airport_name, fcst_date_str)    
 
     # Load data if not present
-    if not data_loaded:
+    if data_loaded:
+        print('AvF data found in db for {0} on {1}'.format(airport_name, fcst_date_str))
+    else:
+        print('Updating AvF for {0} on {1}'.format(airport_name, fcst_date_str))
+        
         try:
             df_avf_compare = get_avf_compare(airport_name, fcst_date_str)
             df_avf_compare['airport_name'] = airport_name
 
+            
             for c in columns:
                 if c not in df_avf_compare.columns:
                     df_avf_compare[c] = np.nan
@@ -206,4 +220,64 @@ def update_nws_avf_compare_db(db_conn, airport_name, fcst_date_str):
             print(('something went wrong loading avf compare ' + 
                    'for {0} for comparison date {1}'.format(airport_name, fcst_date_str)))
 
+def update_actl_db_from_disk(db_conn, data_input_root, airport_list, actl_dt):
+    # Load data for each airport
+    for airport_name in airport_list:    
+        # Expected location for the airport's forecast
+        airport_actl_path = os.path.join(
+            data_input_root,
+            airport_name,
+            'nws_actual_{0}.csv'.format(actl_dt)
+        )
 
+        # May have gzipped it
+        if not os.path.exists(airport_actl_path):
+            airport_actl_path = airport_actl_path + '.gz'
+
+        # Update dB
+        if os.path.exists(airport_actl_path):
+            df_nws_actl = pd.read_csv(airport_actl_path)
+            update_nws_actl_db(db_conn, airport_name, df_nws_actl, actl_dt)
+
+def update_fcst_db_from_disk(db_conn, data_input_root, airport_list, pull_dt):
+    # Load data for each airport
+    for airport_name in airport_list:    
+        # Expected location for the airport's forecast
+        airport_fcst_path = os.path.join(
+            data_input_root,
+            airport_name,
+            'nws_fcst_{0}.csv'.format(pull_dt)
+        )
+
+        # May have gzipped it
+        if not os.path.exists(airport_fcst_path):
+            airport_fcst_path = airport_fcst_path + '.gz'
+
+        # Update dB
+        if os.path.exists(airport_fcst_path):
+            df_nws_fcst = pd.read_csv(airport_fcst_path)
+            update_nws_fcst_db(db_conn, airport_name, df_nws_fcst, pull_dt)
+
+
+def update_fcst_db_from_disk(db_conn, data_input_root, airport_list, pull_dt):
+    # Load data for each airport
+    for airport_name in airport_list:    
+        # Expected location for the airport's forecast
+        airport_fcst_path = os.path.join(
+            data_input_root,
+            airport_name,
+            'nws_fcst_{0}.csv'.format(pull_dt)
+        )
+
+        # May have gzipped it
+        if not os.path.exists(airport_fcst_path):
+            airport_fcst_path = airport_fcst_path + '.gz'
+
+        # Update dB
+        if os.path.exists(airport_fcst_path):
+            df_nws_fcst = pd.read_csv(airport_fcst_path)
+            update_nws_fcst_db(db_conn, airport_name, df_nws_fcst, pull_dt)
+
+def update_avf_db_from_list(db_conn, airport_list, fcst_date_str):
+    for airport_name in airport_list:        
+        update_nws_avf_compare_db(db_conn, airport_name, fcst_date_str)                            
